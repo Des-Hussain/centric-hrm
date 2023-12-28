2c09129faafa9cff9d189e4dd5b562e18a527e73
Mohammad Hussain <mohammadhussainmobeen@gmail.com>
2023-12-28 15:41:26 +0500

    auth screens

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppRoutes } from '../utils/resources/routes';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: AppRoutes.login,
+    component: LoginComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { OtpVerificationComponent } from './otp-verification/otp-verification.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    ForgotPasswordComponent,
+    OtpVerificationComponent,
+    ChangePasswordComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
