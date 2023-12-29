6adf149bb81d4c2962bf385deadc0883e8f6ecda
Ahmed <63443985+dev1942@users.noreply.github.com>
2023-12-29 21:16:09 +0500

    login WIP

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,18 +6,20 @@ import { LoginComponent } from './login/login.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { OtpVerificationComponent } from './otp-verification/otp-verification.component';
 import { ChangePasswordComponent } from './change-password/change-password.component';
+import { TextfieldComponent } from "../utils/widgets/textfield/textfield.component";
 
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    ForgotPasswordComponent,
-    OtpVerificationComponent,
-    ChangePasswordComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+    declarations: [
+        LoginComponent,
+        ForgotPasswordComponent,
+        OtpVerificationComponent,
+        ChangePasswordComponent
+    ],
+    imports: [
+        CommonModule,
+        AuthRoutingModule,
+        TextfieldComponent
+    ]
 })
 export class AuthModule { }
