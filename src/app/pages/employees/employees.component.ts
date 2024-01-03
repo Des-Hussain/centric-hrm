3ada6cfbcf9fe7577b689536f7cabf3cf978ea7b
Ahmed <63443985+dev1942@users.noreply.github.com>
2024-01-03 22:08:36 +0500

    employees wip

--- src/app/pages/employees/employees.component.ts
+++ src/app/pages/employees/employees.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AttendanceConModel } from 'src/app/domain/interfaces/attendance_con_model';
 
 @Component({
   selector: 'app-employees',
@@ -7,4 +8,54 @@ import { Component } from '@angular/core';
 })
 export class EmployeesComponent {
 
+   name: string = "Abdul Moiz";
+
+  attendanceContainer: AttendanceConModel[] = [
+       {
+     title: "Total Employees",
+     color: "#00D4E7",
+     icon: "./../../../../assets/icons/total_employees.svg",
+     subTitle: "2",
+     action: "none"
+   },
+    {
+     title: "New Employees",
+     color: "#00D4E7",
+     icon: "./../../../../assets/icons/present.svg",
+     subTitle: "52",
+     action: "none"
+   },
+   {
+     title: "On Time",
+     color: "#FF5C58",
+     icon: "./../../../../assets/icons/absent.svg",
+     subTitle: "12",
+     action: "none"
+   },
+   {
+     title: "Late",
+     color: "#FFB800",
+     icon: "./../../../../assets/icons/hourglass.svg",
+     subTitle: "4",
+     action: "none"
+   },
+   {
+     title: "Absent",
+     color: "#FFB800",
+     icon: "./../../../../assets/icons/hourglass.svg",
+     subTitle: "4",
+     action: "none"
+   },
+   {
+    title: "Leave",
+    color: "#9BA5B7",
+    icon: "./../../../../assets/icons/total_employees.svg",
+    subTitle: "2",
+    action: "none"
+  },
+
+  
+ 
+ 
+ ];
 }
